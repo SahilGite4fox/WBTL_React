900e66b887eecea87260410e7e2c2f7fab1bb258
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-07-17 15:13:51 +0530

    react deploy setup

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/WBTL_React">
       <App />
     </BrowserRouter>
   </React.StrictMode>
