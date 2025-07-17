b238981db70a1835ac6579c5d65714766a6331e4
SahilGite4fox <sahilgite.4fox@gmail.com>
2025-07-17 15:28:39 +0530

    repo Rename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/WBTL_React">
+    <BrowserRouter basename="/wbtl_react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
